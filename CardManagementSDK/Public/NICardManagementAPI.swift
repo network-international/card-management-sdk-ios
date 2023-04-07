03bb84ee4ee9ccc3399a811412b980e7dcbe2a68
Gabriel Cernestean <gabriel.cernestean@endava.com>
2023-04-07 11:21:20 +0300

    Verify PIN feature added.

--- CardManagementSDK/Public/NICardManagementAPI.swift
+++ CardManagementSDK/Public/NICardManagementAPI.swift
@@ -36,6 +36,19 @@ import UIKit
         FormCoordinator(navigationController: viewController, route: .setPin(input: input, type: type), completion: completion).start()
     }
     
+    @objc public static func verifyPinForm(input: NIInput, viewController: UIViewController, completion: @escaping (NISuccessResponse?, NIErrorResponse?, @escaping () -> Void) -> Void) {
+        setupDisplayAttributes(input.displayAttributes)
+        verifyPinForm(input: input, type: .dynamic, viewController: viewController, completion: completion)
+    }
+    
+    @objc public static func verifyPinForm(input: NIInput, type: NIPinFormType, viewController: UIViewController, completion: @escaping (NISuccessResponse?, NIErrorResponse?, @escaping () -> Void) -> Void) {
+        if viewController is UINavigationController {
+            completion(nil, NIErrorResponse(error: NISDKErrors.NAV_ERROR)){}
+        }
+        setupDisplayAttributes(input.displayAttributes)
+        FormCoordinator(navigationController: viewController, route: .verifyPin(input: input, type: type), completion: completion).start()
+    }
+    
     @objc public static func changePinForm(input: NIInput, viewController: UIViewController, completion: @escaping (NISuccessResponse?, NIErrorResponse?, @escaping () -> Void) -> Void) {
         setupDisplayAttributes(input.displayAttributes)
         changePinForm(input: input, type: .dynamic, viewController: viewController, completion: completion)
@@ -56,6 +69,10 @@ import UIKit
     
     @objc public static func setPin(pin: String, input: NIInput, completion: @escaping (NISuccessResponse?, NIErrorResponse?, @escaping () -> Void) -> Void) {
         shared.setPin(pin, input: input, completion: completion)
+    }
+    
+    @objc public static func verifyPin(pin: String, input: NIInput, completion: @escaping (NISuccessResponse?, NIErrorResponse?, @escaping () -> Void) -> Void) {
+        shared.verifyPin(pin, input: input, completion: completion)
     }
     
     @objc public static func changePin(oldPin: String, newPin: String, input: NIInput, completion: @escaping (NISuccessResponse?, NIErrorResponse?, @escaping () -> Void) -> Void) {
@@ -81,7 +98,18 @@ import UIKit
             if let error = error {
                 completion(nil, error){}
             } else if response != nil {
-                let result = NISuccessResponse(message: "Pin set successful!")
+                let result = NISuccessResponse(message: "Pin set successfully!")
+                completion(result, nil){}
+            }
+        }
+    }
+    
+    private func verifyPin(_ pin: String, input: NIInput, completion: @escaping (NISuccessResponse?, NIErrorResponse?, @escaping () -> Void) -> Void) {
+        PinManager().verifyPin(pin, input: input) { response, error in
+            if let error = error {
+                completion(nil, error){}
+            } else if response != nil {
+                let result = NISuccessResponse(message: "Pin is correct!")
                 completion(result, nil){}
             }
         }
@@ -92,7 +120,7 @@ import UIKit
             if let error = error {
                 completion(nil, error){}
             } else if response != nil {
-                let result = NISuccessResponse(message: "Pin changed successfuly!")
+                let result = NISuccessResponse(message: "Pin changed successfully!")
                 completion(result, nil){}
             }
         }
--- CardManagementSDK/Source/Network/Managers/PinManager.swift
+++ CardManagementSDK/Source/Network/Managers/PinManager.swift
@@ -36,6 +36,33 @@ class PinManager {
         }
     }
     
+    func verifyPin(_ pin: String, input: NIInput, completion: @escaping (Response?, NIErrorResponse?) -> Void) {
+        /// 1. get clear pan
+        getClearPan(input) { clearPan in
+            guard let clearPan = clearPan else {
+                completion(nil, NIErrorResponse(error: NISDKErrors.NETWORK_ERROR))
+                return
+            }
+            /// 2. get pin certificate
+            self.getPinCertificate(input: input) { certificate in
+                guard let certificate = certificate else {
+                    completion(nil, NIErrorResponse(error: NISDKErrors.NETWORK_ERROR))
+                    return
+                }
+                /// 3. create and encrypt pin block
+                let pinEncryption = PinEncryption(pin: pin, pan: clearPan, certificate: certificate)
+                /// 4. set pin
+                NIMobileAPI.shared.verifyPin(pinEncryption, input: input) { response, error in
+                    if let error = error {
+                        completion(nil, error)
+                    } else if response != nil {
+                        completion(response, nil)
+                    }
+                }
+            }
+        }
+    }
+    
     func changePin(oldPin: String, newPin: String, input: NIInput, completion: @escaping (Response?, NIErrorResponse?) -> Void) {
         /// 1. get clear pan
         getClearPan(input) { clearPan in
