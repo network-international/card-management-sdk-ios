32ad234ea4b8b2326d7bf50cae7508d97cb30bdf
Aleksei Kiselev <aleksei.kiselev@network.global>
2026-04-23 12:11:33 +0400

    [2.1.7] Fix response fileds deserialization.

--- CardManagementSDK/Source/Network/Parameters/PinParams.swift
+++ CardManagementSDK/Source/Network/Parameters/PinParams.swift
@@ -29,7 +29,7 @@ struct PinParams: Codable {
     }
     
     enum CodingKeys: String, CodingKey {
-        case cardIdentifierId = "00000000"
+        case cardIdentifierId = "card_identifier_id"
         case cardIdentifierType = "card_identifier_type"
 //        case cardSequenceNumber = "card_sequence_number"
 //        case cardExpiryDate = "card_expiry_date"
